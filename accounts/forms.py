98993e40d4205b69ab9985fce56e5f1924a2e4ac
cronugs <cronugs@outlook.com>
2019-12-28 13:24:14 +0100

    added forms for customers and clinics. Added views to register customers clinics and to log in and out

--- accounts/forms.py
+++ accounts/forms.py
@@ -20,3 +20,27 @@ class PatientRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     phone = PhoneNumberField(label = "Phone Number")
     personnummer = forms.CharField(label = "Personnummer")
+
+
+class CustomerRegistrationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password Confirmation',
+        widget=forms.PasswordInput
+    )
+    fname = forms.CharField(label="First Name")
+    lname = forms.CharField(label="Last Name")
+
+    email = forms.EmailField(required=True)
+    phone = PhoneNumberField(label = "Phone Number")
+    personnummer = forms.CharField(label = "Personnummer")
+
+
+class ClinicRegistrationForm(forms.Form):
+    clinic_name = forms.CharField(label='Clinic Name')
+    street_address1 = forms.CharField(label='Street address 1')
+    street_address2 = forms.CharField(label='Street address 2')
+    county = forms.CharField(label='County')
+    city = forms.CharField(label='City')
+    phone = PhoneNumberField(label='Phone')
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,82 @@
-from django.shortcuts import render
-from .forms import PatientRegistrationForm, UserLoginForm
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages, auth
+from .forms import PatientRegistrationForm, CustomerRegistrationForm, ClinicRegistrationForm, UserLoginForm
 
-# Create your views here.
+
 def register_user(request):
-    form = PatientRegistrationForm
-    return render(request, "register.html", {"form": form})
+    """
+    A view for regular users to register
+    """
+    if request.method == 'POST':
+        user_form = PatientRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "You are not able to log in at this time")
+    else:
+        user_form = PatientRegistrationForm()
+
+    context = {'user_form': user_form}
+    return render(request, "register.html", context)
+
+
+def register_customer(request):
+    """
+    A view for customers to register
+    """
+    if request.method == 'POST':
+        customer_form = UserLoginForm(request.POST)
+        if customer_form.is_valid():
+            customer_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered a clinic")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "You are not able to log in at this time")
+    else:
+        customer_form = CustomerRegistrationForm()
+
+    context = {'customer_form': customer_form}
+    return render(request, "register_customer.html", context)
+
+
+def register_clinic(request):
+    """
+    A view to handle registering clinics
+    """
+    if request.method == 'POST':
+        clinic_form = ClinicRegistrationForm(request.POST)
+        if clinic_form.is_valid():
+            clinic_form.save()
+    else:
+        clinic_form = ClinicRegistrationForm()
+    
+    context = {'clinic_form': clinic_form}
+    return render(request, "register_clinic.html", context)
 
 
 def login(request):
     form = UserLoginForm
     return render(request, "login.html", {"form": form})
+
+
+def logout(request):
+    """A view that logs the user out and redirects back to the index page"""
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
